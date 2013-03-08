ee06d3c39a06f4cef054a667ca20e9711cca27f2
patrick aubin <paubin12@gmail.com>
2013-03-08 11:37:57 -0600

    added basic strutural elements

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='',
+    version='',
+    author='',
+    author_email='',
+    packages=['package'],
+    include_package_data=True,
+    url=''
+    description='',
+)
+
